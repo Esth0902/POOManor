da280c118db86b2097fdacdf889c6b0fd10f1d49
Laetitia <laetitia.voue@gmail.com>
2024-11-30 12:44:26 +0100

    Commit - Laeti - Map

--- POOManor/Pages/Map.razor.cs
+++ POOManor/Pages/Map.razor.cs
@@ -1,21 +1,7 @@
-@page "/Map"
+using Microsoft.AspNetCore.Components;
 
-    <h3>Map</h3>
-    <div class="map">
-    @for (int row = 0; row < 15; row++)
+namespace POOManor.Pages;
+
+public partial class Map : ComponentBase
 {
-    <div class="row">
-        @for (int col = 0; col < 20; col++)
-    {
-        @if (row == 7 && col == 10)
-        {
-            <div class="cell cell1"></div> <!-- Cellule spéciale -->
-        }
-        else
-        {
-            <div class="cell"></div> <!-- Cellule normale -->
-        }
-    }
-    </div>
 }
-</div>
