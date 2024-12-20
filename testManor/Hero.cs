26f30eabd989e0449418826abf475e120c197086
Laetitia <laetitia.voue@gmail.com>
2024-12-20 15:56:45 +0100

    commit - Hero/GestionVie fonctionne pas

--- testManor/Hero.cs
+++ testManor/Hero.cs
@@ -1,39 +1,39 @@
-namespace testManor.Pages;
+//namespace testManor.Pages;
 
-public class Heros(string name, int positionX, int positionY)
-{
+//public class Heros(string name, int positionX, int positionY)
+//{
     
-    public string? Name { get; set; } = name;
-    public int? PositionX { get; set; } = positionX;
-    public int? PositionY { get; set; } = positionY;
-    public virtual string? Image { get; }
+    //public string? Name { get; set; } = name;
+    //public int? PositionX { get; set; } = positionX;
+    //public int? PositionY { get; set; } = positionY;
+    //public virtual string? Image { get; }
 
-    protected int? Life { get; set; }
-    protected int? Damage { get; set; }
+    //protected int? Life { get; set; }
+    //protected int? Damage { get; set; }
         
-    protected int? Armor { get; set; }
+    //protected int? Armor { get; set; }
 
 
-    public void Attacked(int damage)
-    {
-        Life -= damage;
-    }
+    //public void Attacked(int damage)
+    //{
+       // Life -= damage;
+   // }
 
-    public bool Alive()
-    {
-        return Life > 0;
-    }
+    //public bool Alive()
+    //{
+       // return Life > 0;
+    //}
     
-}
+//}
 
-public class Hero : Heros
-{
-    public override string Image => "images/HeroF.png";
+//public class Hero : Heros
+//{
+    //public override string Image => "images/HeroF.png";
     
-    public Hero(string name, int positionX, int positionY) : base(name, positionX, positionY)
-    {
-        Life = 5;
-        Armor = 0;
-        Damage = 2;
-    }
-}
+    //public Hero(string name, int positionX, int positionY) : base(name, positionX, positionY)
+  //  {
+      //  Life = 5;
+        //Armor = 0;
+        //Damage = 2;
+    //}
+//}
---
+++ testManor/MonHero.cs
@@ -0,0 +1,20 @@
+﻿namespace testManor.Pages;
+
+public class MonHero
+{
+    public string Nom { get; set; }
+    public string Image { get; set; } 
+    public int Damage { get; set; }
+    public int Life { get; set; }
+    public int Armor { get; set; }
+    
+    public MonHero(string nom, string image, int damage, int life, int armor)
+    {
+        Nom = nom;
+        Image = image;
+        Damage = damage;
+        Life = life;
+        Armor = armor;
+    }
+}
+
