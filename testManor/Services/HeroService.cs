7bbb4ccd423c48c94d60e85b02535daf8bd968b9
Laetitia <laetitia.voue@gmail.com>
2025-01-02 17:13:41 +0100

    Page GameOver et Evenement si <= 0 points de vie

--- testManor/Services/HeroService.cs
+++ testManor/Services/HeroService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Immutable;
+using Microsoft.AspNetCore.Components;
 
 namespace testManor.Pages.Services;
 
@@ -8,11 +9,27 @@ public class HeroService
    
    public List<string> ListInventaire { get; set; } = [];
    public event Action? OnChange;
+   
+   private readonly NavigationManager nav;
+
+   public HeroService(NavigationManager nav)
+   {
+      this.nav = nav;
+   }
 
    public void AddNotif(string item)
    {
       ListInventaire.Add(item);
       OnChange?.Invoke();
+   }
+   
+   public void CheckGameOver()
+   {
+      if (MonHero.Life <= 0)
+      {
+         // Déclencher le Game Over
+         nav.NavigateTo("MapGameOver");
+      }
    }
 }
  
