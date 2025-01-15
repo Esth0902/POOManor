7c99972d495f19bb2630a34b62df3959d2dbb89c
Esth0902 <esther.stassin@gmail.com>
2025-01-15 20:28:18 +0100

    stop moving à false quand on clique sur "jouons"

--- testManor/Services/HeroService.cs
+++ testManor/Services/HeroService.cs
@@ -5,7 +5,8 @@ namespace testManor.Pages.Services;
 public class HeroService   
 {
    public MonHero MonHero { get; set; }
-   
+
+   public bool StopMoving { get; set; } = true; //arrêt des déplacements des monstres initialisé à true, passe à false quand on clique sur "jouons"
    public List<string> ListInventaire { get; set; } = [];
    public event Action? OnChange;
 
