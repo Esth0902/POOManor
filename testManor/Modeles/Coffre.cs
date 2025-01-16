90484f5a42628e2391a82a9f987a1f9223eedbe1
Laetitia <laetitia.voue@gmail.com>
2025-01-16 20:11:41 +0100

    Commentaire - Coffre

--- testManor/Modeles/Coffre.cs
+++ testManor/Modeles/Coffre.cs
@@ -12,22 +12,26 @@ public class Coffre
     public string ImageOuvert = "images/coffreouvert.png";
     
     public Coffre(int positionX, int positionY)
+    //Constructeur de la classe Coffre
     {
         PositionX = positionX;
         PositionY = positionY;
-        
+    //Initialisation de la liste des objets     
         coffrealeatoires = new List<Item>();
+    //Ajout des armures, armes et potions à la liste d'objets    
         coffrealeatoires.AddRange(Armure.ListArmures());
         coffrealeatoires.AddRange(Arme.ListArme());
         coffrealeatoires.AddRange(Potion.ListPotions());
 
     }
 
-    public object ChoisirObjet()
+    public object ChoisirObjet() 
+    //Methode pour choisir un objet aléatoire dans le coffre
     {
-        Random aleatoire = new Random();
-        int index = aleatoire.Next(coffrealeatoires.Count);
-        return coffrealeatoires[index];
+        Random aleatoire = new Random(); // Génère un nombre aléatoire
+        int index = aleatoire.Next(coffrealeatoires.Count); 
+        // Next va générer entre 0 et X (nombre d'éléments dans le coffre avec le count)
+        return coffrealeatoires[index]; //Retourne un element aléatoire dans le coffre 
     }
 }
 
